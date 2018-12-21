a3d4020a2d9844160803c3747b0616aaafa60b89
Falcon <20vampir11@gmail.com>
2018-12-21 13:45:30 +0300

    fix sub

--- lib.asm
+++ lib.asm
@@ -16,10 +16,10 @@ ret
 substraction:
     push ebx
     mov ebx,[esp + 12]
-    movdqu xmm0,[ebx]
-    mov ebx,[esp + 8]
     movdqu xmm1,[ebx]
-    subps xmm1,xmm0
+    mov ebx,[esp + 8]
+    movdqu xmm0,[ebx]
+    subps xmm0,xmm1
     movdqu [ebx],xmm0
     pop ebx
 ret
